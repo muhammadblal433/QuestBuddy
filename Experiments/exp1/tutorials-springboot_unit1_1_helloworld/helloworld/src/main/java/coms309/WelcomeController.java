3d8122e4196e29c2ac38bf8477824328b3355d36
Muhammad Blal <mblal@iastate.edu>
2025-09-07 11:57:14 -0500

    for exp1: changed to JDK 17 version, changed server port to 8081, added new JSON endpoints - including 2 in a new file called HelloController.java

--- Experiments/exp1/tutorials-springboot_unit1_1_helloworld/helloworld/src/main/java/coms309/WelcomeController.java
+++ Experiments/exp1/tutorials-springboot_unit1_1_helloworld/helloworld/src/main/java/coms309/WelcomeController.java
@@ -7,11 +7,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 @RestController
 class WelcomeController {
 
+    // GET http://localhost:8081/
     @GetMapping("/")
     public String welcome() {
         return "Hello and welcome to COMS 309";
     }
-    
+
+    // GET http://localhost:8081/Muhammad
     @GetMapping("/{name}")
     public String welcome(@PathVariable String name) {
         return "Hello and welcome to COMS 309: " + name;
