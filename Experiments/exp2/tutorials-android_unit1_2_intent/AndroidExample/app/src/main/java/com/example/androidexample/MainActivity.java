03310071091d63c2150dede4f5751518c64c3e4b
Aniroop Naladala <aniroopnaladala37@gmail.com>
2025-09-08 19:24:54 -0500

    exp2: used ActivityResultLauncher API to return counter value to MainActivity

--- Experiments/exp2/tutorials-android_unit1_2_intent/AndroidExample/app/src/main/java/com/example/androidexample/MainActivity.java
+++ Experiments/exp2/tutorials-android_unit1_2_intent/AndroidExample/app/src/main/java/com/example/androidexample/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.androidexample;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -14,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView messageText;     // define message textview variable
     private Button counterButton;     // define counter button variable
+    private TextView tvLastResult;
+    private ActivityResultLauncher<Intent> counterLauncher; // I inserted an API to receive the data back
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +27,18 @@ public class MainActivity extends AppCompatActivity {
         /* initialize UI elements */
         messageText = findViewById(R.id.main_msg_txt);      // link to message textview in the Main activity XML
         counterButton = findViewById(R.id.main_counter_btn);// link to counter button in the Main activity XML
+        tvLastResult = findViewById(R.id.tvLastResult);
+
+        counterLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if(result.getResultCode() == RESULT_OK && result.getData() != null){
+                        int finalCount = result.getData()
+                                .getIntExtra(CounterActivity.EXTRA_FINAL_COUNT, 0);
+                        tvLastResult.setText("Last Result: " + finalCount);
+                    }
+                }
+        );
 
         /* extract data passed into this activity from another activity */
         Bundle extras = getIntent().getExtras();
@@ -38,9 +54,10 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                /* when counter button is pressed, use intent to switch to Counter Activity */
+                /* When we click on the counter button press, we create intent to open CounterActivity, start with default value of 0, and use the API to get the result back*/
                 Intent intent = new Intent(MainActivity.this, CounterActivity.class);
-                startActivity(intent);
+                intent.putExtra(CounterActivity.EXTRA_START_COUNT, 0);
+                counterLauncher.launch(intent);
             }
         });
     }
