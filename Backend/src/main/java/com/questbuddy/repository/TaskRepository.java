225cbc0f45bd90ff17d7827995a31b3d680beff6
Muhammad Blal <mblal@iastate.edu>
2025-10-05 10:44:24 -0500

    implemented the interface TaskRepository.java which provides database access methods for the Task entity. It extends JpaRepository, so Spring Boot automatically generates standard CRUD operations like save(), findAll(), and deleteById(). The custom method findByUserId(Long userId) allows filtering tasks belonging to a specific user

--- Backend/src/main/java/com/questbuddy/repository/TaskRepository.java
+++ Backend/src/main/java/com/questbuddy/repository/TaskRepository.java
@@ -1,4 +1,9 @@
 package com.questbuddy.repository;
 
-public class TaskRepository {
+import com.questbuddy.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface TaskRepository extends JpaRepository<Task, Long> {
+    List<Task> findByUserId(Long userId);
 }
