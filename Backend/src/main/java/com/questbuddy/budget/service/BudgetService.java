52ca388931559db3d0b83ad73b43debe676d0700
Muhammad Blal <mblal@iastate.edu>
2025-11-03 14:41:56 -0600

    in BudgetService.java, we exposed update(ownerId, budgetId, BudgetUpdateDTO)

--- Backend/src/main/java/com/questbuddy/budget/service/BudgetService.java
+++ Backend/src/main/java/com/questbuddy/budget/service/BudgetService.java
@@ -8,6 +8,7 @@ public interface BudgetService {
     BudgetResponseDTO create(Long ownerId, BudgetCreateDTO body);
     List<BudgetResponseDTO> list(Long ownerId);
     BudgetResponseDTO get(Long ownerId, Long budgetId);
+    BudgetResponseDTO update(Long ownerId, Long budgetId, BudgetUpdateDTO body);
     void delete(Long ownerId, Long budgetId);
     List<BudgetBalanceDTO> balances(Long ownerId, Long budgetId);
 }
