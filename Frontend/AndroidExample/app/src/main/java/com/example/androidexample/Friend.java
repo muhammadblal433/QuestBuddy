7a2b42765d5596256003b39347facd8d9ff21462
Aniroop Naladala <aniroopnaladala37@gmail.com>
2025-11-04 00:04:13 -0600

    had to delete extra files that are not needed

--- Frontend/AndroidExample/app/src/main/java/com/example/androidexample/Friend.java
+++ Frontend/AndroidExample/app/src/main/java/com/example/androidexample/Friend.java
@@ -8,6 +8,7 @@ public class Friend {
     private String status; // current status
     private boolean incoming; // true if it's an incoming request
     private int mutualCount; // number of mutual friends
+    private String email; // email
 
     public long getId() { return id; } // get id
     public void setId(long id) { this.id = id; } // set id
@@ -20,6 +21,10 @@ public class Friend {
 
     public String getStatus() { return status; } // get user status
     public void setStatus(String status) { this.status = status; } // set user status
+
+    public String getEmail() { return email; } // get email
+
+    public void setEmail(String email) { this.email = email; } // set email
 
     public int getMutualCount() { return mutualCount; } // get mutual count
     public void setMutualCount(int mutualCount) { this.mutualCount = mutualCount; } // set mutual count
