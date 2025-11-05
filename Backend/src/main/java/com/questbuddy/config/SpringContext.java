46c48d37fe66f82b2ded567e88b79cc5e8355201
ayaanmusasyed <ayaanmusasyed@gmail.com>
2025-11-04 22:05:23 -0600

    Added SpringContext.java so that we can use beans like service layers and repos. Also started TripChatEndpoint.java (with only basic layer of one way push msgs).

--- Backend/src/main/java/com/questbuddy/config/SpringContext.java
+++ Backend/src/main/java/com/questbuddy/config/SpringContext.java
@@ -1,4 +1,26 @@
 package com.questbuddy.config;
 
-public class SpringContext {
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * Exposes Spring's ApplicationContext statically so container-managed endpoints (@ServerEndpoint) can resolve Spring beans (services, mappers, etc.).
+ */
+@Component
+public class SpringContext implements ApplicationContextAware {
+    private static ApplicationContext context;
+
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        context = applicationContext;
+    }
+
+
+    public static <T> T getBean(Class<T> type) {
+        return context.getBean(type);
+    }
 }
